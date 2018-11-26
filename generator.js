194068f203c559b7552ee191206055240ddaa8ea
Hanxiu Chen <hichenhanxiu@gmail.com>
2018-11-27 00:09:20 +1100

    Update generator.js

--- generator.js
+++ generator.js
@@ -14,7 +14,7 @@ function generateRainbow() {
 }
 
 function randomData(elementId,dataName) {
-    var t1Num = Math.floor(Math.random());
+    var t1Num = Math.floor(Math.random()*dataName.length);
     var text1 = document.getElementById(elementId);
     text1.innerHTML = dataName[t1Num];    
 }
